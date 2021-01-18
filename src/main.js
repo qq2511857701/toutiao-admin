4fc4f4515b199fd4eced481feb0349d6cec00e68
tianqi.zhang <2511857701.com>
2021-01-18 13:21:29 +0800

    项目结构初始化

--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-Vue.config.productionTip = false
+// 加载全局样式文件
 
+import styles from './styles/index.less'
+
+Vue.config.productionTip = false
+// 创建vue根实例
+// 把router配置到跟实例中
+// 通过render方法吧App跟组件渲染到app人口节点
 new Vue({
   router,
   store,
